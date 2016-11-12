e981eb217b83cb5d5ba46cbe08097f69578bec50
MiniBeast\Joe <joefernandez88@gmail.com>
2016-11-12 17:35:14 -0500

    BT20 - Added TankTracks for movement of tank

---
+++ BattleTank/Source/BattleTank/Private/TankTracks.cpp
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleTank.h"
+#include "TankTracks.h"
+
+
+
+void UTankTracks::SetThrottle(float Throttle)
+{
+	// auto Time = GetWorld()->GetTimeSeconds();
+	auto Name = GetName();
+	UE_LOG(LogTemp, Warning, TEXT("%s throttle: %f"), *Name, Throttle);
+
+	// TODO clamp throttle so player cannot over-drive
+	auto ForceApplied = GetForwardVector() * Throttle * TankMaxDrivingForce;
+	auto ForceLocation = GetComponentLocation();
+	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
+}
+
---
+++ BattleTank/Source/BattleTank/Public/TankTracks.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Components/StaticMeshComponent.h"
+#include "TankTracks.generated.h"
+
+/*
+* TankTracks is used to set a maximum driving force and to apply forces to the tank.
+*/
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class BATTLETANK_API UTankTracks : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+	
+public:
+	// Sets a throttle between -1 and +1
+	UFUNCTION(BlueprintCallable, Category = Input)
+	void SetThrottle(float Throttle);
+	
+	// Max force per track in Newtons
+	UPROPERTY(EditDefaultsOnly, Category = Setup)
+	float TankMaxDrivingForce = 400000; // Assume 40 ton tank and 1g acceleration
+};
